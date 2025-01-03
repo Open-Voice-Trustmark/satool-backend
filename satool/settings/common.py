32ff22e822c72cf15cae13a6efc5e67b8b57eb67
Luis Santos <luis@spartanbits.com>
2025-01-03 13:47:04 +0100

    Fix empty auth refresh token issue

--- satool/settings/common.py
+++ satool/settings/common.py
@@ -162,6 +162,7 @@ REST_FRAMEWORK = {
 
 REST_AUTH = {
     "USE_JWT": True,
+    "JWT_AUTH_HTTPONLY": False,
     "JWT_AUTH_COOKIE": "jwt-auth",
     "JWT_AUTH_REFRESH_COOKIE": "jwt-auth-refresh",
     "REGISTER_SERIALIZER": "accounts.serializers.CustomRegisterSerializer",
